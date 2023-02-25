2d11eba68eb5d5acf067a446ab8e3750ae4b38a8
erivaldo junior <erivaldo.junior@referenceit.com.br>
2023-02-25 20:07:49 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<h1 class="text-red-500">cafecomcodigo</h1>
+<button class="btn btn-primary">Button</button>
